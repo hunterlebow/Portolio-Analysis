1d384522a82afbe61410a063b7f6469e0e0d1fa4
Hunter Lebow <hunterlebow@gmail.com>
2023-12-28 20:18:42 -0500

    Markowitz Efficient Frontier Added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Find sharpe ratio using monte carlo simulation and visualize the simulation with an efficient frontier and simulation statistics
+Explore Sharpe Ratio potential of your portfolio through monte carlo simulated investment weights
+Visualize your portfolio's simulation against an Markowitz Efficient Frontier
+Discover the optimal weights of each of stock in your portfolio that yielded the highest Sharpe Ratio
+
+The Sharpe Ratio explains how many units of excess return will be achieved above the risk free rate per one unit of risk as defined by the standard deviation of your portfolio.
+A sharpe ratio below 0.0 means your portfolio is being outperformed by the risk free rate
+A sharpe ratio between 0.0 and 0.5 leaves much room for improvement in your portfolio
+A sharpe ratio betweeen 0.5 and 1.0 has value but leaves room for improvement
+A sharpe ratio above above 1.0 is good
+A sharpe ratio above 2.0 is great
+A sharpe ratio above 3.0 is exceptional
--- src/sharpe_ratio/simulation.py
+++ src/sharpe_ratio/simulation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
 
 class Simulation():
@@ -20,6 +21,33 @@ class Simulation():
     def get_covariance_matrix(self):
         r = self.get_pct_returns()
         return r.cov()
+    
+    # Additional method to calculate portfolio volatility for given weights
+    def portfolio_volatility(self, weights, mean_returns, cov_matrix):
+        return np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights))) * np.sqrt(self.TRADING_DAYS)
+
+    # Additional method to minimize volatility for a given target return
+    def minimize_volatility(self, target_return, mean_returns, cov_matrix):
+        num_assets = len(mean_returns)
+        args = (mean_returns, cov_matrix)
+        constraints = (
+            {'type': 'eq', 'fun': lambda x: np.sum(x) - 1},  # Weights must sum to 1
+            {'type': 'eq', 'fun': lambda x: np.dot(x, mean_returns) - target_return}  # Targeted return
+        )
+        bounds = tuple((0, 1) for asset in range(num_assets))
+        result = minimize(self.portfolio_volatility, num_assets * [1. / num_assets], args=args,
+                          method='SLSQP', bounds=bounds, constraints=constraints)
+        return result
+
+    # Method to calculate the efficient frontier
+    def efficient_frontier(self, mean_returns, cov_matrix, return_range):
+        efficient_portfolios = []
+        for ret in return_range:
+            efficient_portfolio = self.minimize_volatility(ret, mean_returns, cov_matrix)
+            if efficient_portfolio.success:
+                efficient_portfolios.append(efficient_portfolio)
+        return efficient_portfolios
+
 
     def simulate(self):
         """
@@ -79,40 +107,68 @@ class Simulation():
         max_sharpe_index = sharpe_ratios.index(max(sharpe_ratios))
         max_sharpe_weights = weights[max_sharpe_index]
 
-        # Calculate cumulative returns
-        daily_returns = self.get_pct_returns()
-        weighted_returns = daily_returns * max_sharpe_weights
-        portfolio_returns = weighted_returns.sum(axis=1)
-        cumulative_returns = (1 + portfolio_returns).cumprod()
-
         # Creating a figure with a 2x2 grid layout
-        fig, axs = plt.subplots(2, 2, figsize=[16, 16])
+        fig, axs = plt.subplots(2, 2, figsize=[16, 16], facecolor="white")
 
         # Plotting the Efficient Frontier on the first subplot (top left)
         sc = axs[0, 0].scatter(vols, returns, c=sharpe_ratios, cmap='RdYlGn')
         axs[0, 0].scatter(vols[max_sharpe_index], returns[max_sharpe_index],
-                          c="black", marker='*', s=500)  # Highlight the max Sharpe ratio
+                          c="#FF7FFF", marker='*', s=500, label="Max Sharpe Ratio")  # Highlight the max Sharpe ratio
         axs[0, 0].set_xlabel('Volatility')
         axs[0, 0].set_ylabel('Return')
         fig.colorbar(sc, ax=axs[0, 0], label='Sharpe Ratio')
-        axs[0, 0].set_title('Efficient Frontier')
+        axs[0, 0].set_title('Monte Carlo Portfolio Weight Simulation')
+        axs[0,0].grid(True)
+        axs[0,0].legend()
 
-        # Adding text for the simulation statistics in the second subplot (top right)
+        #Monte Carlo Simulation Statistics
         axs[0, 1].axis('off')  # Turn off the axis for the text subplot
         max_sharpe_ratio = max(sharpe_ratios)
+        
+        # Format portfolio weights as strings with two decimal points and a percentage sign
+        formatted_weights = pd.Series(max_sharpe_weights * 100, index=self.portfolio.columns).map('{:.2f}%'.format)
+        
         text_str = f'Max Sharpe Ratio: {max_sharpe_ratio:.2f}\n\n'
-        text_str += f'Portfolio Weights:\n{pd.Series(max_sharpe_weights * 100, index=self.portfolio.columns).to_string()}'
-        axs[0, 1].text(0.5, 0.5, text_str, fontsize=12, ha='center', va='center', wrap=True)
+        text_str += 'TOP 1 Portfolio Weights:\n' + formatted_weights.to_string()
 
-        # Plotting cumulative returns on the third subplot (bottom left)
-        axs[1, 0].plot(cumulative_returns)
-        axs[1, 0].set_title(f"Cumulative Returns of the Portfolio")
-        axs[1, 0].set_xlabel("Date")
-        axs[1, 0].set_ylabel("Cumulative Returns")
+        axs[0, 1].text(0.5, 0.5, text_str, fontsize=20, ha='center', va='center', wrap=True)
+
+        
+       # Calculate annualized mean returns and covariance matrix
+        annualized_mean_returns = self.get_annualized_mean_returns()
+        cov_matrix = self.get_covariance_matrix()
+
+        # Determine the range of return values for the efficient frontier
+        min_return = min(simulation_results['returns'])
+        max_return = max(simulation_results['returns'])
+        return_range = np.linspace(min_return, max_return, 100)
+
+        # Calculate the Efficient Frontier
+        efficient_portfolios = self.efficient_frontier(annualized_mean_returns, cov_matrix, return_range)
+        efficient_returns = [np.dot(portfolio.x, annualized_mean_returns) for portfolio in efficient_portfolios]
+        efficient_volatilities = [portfolio.fun for portfolio in efficient_portfolios]
+
+        # Plot the Efficient Frontier on the bottom right subplot (axs[1, 1])
+        axs[1, 0].plot(efficient_volatilities, efficient_returns, 'b--', linewidth=2, label='Efficient Frontier')
+        axs[1, 0].set_title('Markowitz Efficient Frontier')
+        axs[1, 0].set_xlabel('Volatility (Standard Deviation)')
+        axs[1, 0].set_ylabel('Expected Return')
         axs[1, 0].grid(True)
+        axs[1, 0].legend()
+        
+        # Plot the Monte Carlo simulation results
+        sc = axs[1, 1].scatter(simulation_results['volatilities'], simulation_results['returns'], c=simulation_results['sharpe_ratios'], cmap='RdYlGn', label='Monte Carlo')
+        # Highlight the maximum Sharpe ratio from the simulation
+        axs[1, 1].scatter(simulation_results['volatilities'][max_sharpe_index], simulation_results['returns'][max_sharpe_index], c='#FF7FFF', marker='*', s=500, label='Max Sharpe Ratio')
+        # Plot the Markowitz Efficient Frontier curve
+        axs[1, 1].plot(efficient_volatilities, efficient_returns, 'b--', linewidth=2, label='Efficient Frontier')
 
-        # Placeholder for additional plot (bottom right)
-        axs[1, 1].axis('off')  # Currently turned off; replace with another plot as needed
+        axs[1, 1].set_title('Efficient Frontier with Monte Carlo Simulation')
+        axs[1, 1].set_xlabel('Volatility (Standard Deviation)')
+        axs[1, 1].set_ylabel('Expected Annual Return')
+        axs[1, 1].legend()
+        axs[1, 1].grid(True)
 
+        # Show the plot
         plt.tight_layout()
         plt.show()
